3756bdfddc599ca7bdab5be6f2b40c5064bc0325
juancrfig <juancrfig@gmail.com>
2025-05-08 22:25:58 -0500

    Add project folder structure

---
+++ src/main/java/com/juanes/examen/Examen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.juanes.examen;
+
+/**
+ *
+ * @author camper
+ */
+public class Examen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
